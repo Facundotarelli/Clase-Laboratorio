bab1468d5269fedce816558489b9742ec3a7374d
Facundotarelli <tarellifacundo@gmail.com>
2022-04-01 10:09:51 -0300

    tarea

---
+++ ClaseApoyo1/src/ClaseApoyo1.c
@@ -0,0 +1,33 @@
+/*
+ ============================================================================
+ Name        : ClaseApoyo1.c
+ Author      : Facundo
+ Version     :
+ Copyright   : MIT
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+	int numero;
+	char respuesta;
+
+	respuesta = 's';
+
+	while(respuesta == 's' )
+	{
+
+
+
+
+	}
+
+
+
+
+	return EXIT_SUCCESS;
+}
---
+++ LaboratorioTarea/src/LaboratorioTarea.c
@@ -0,0 +1,20 @@
+/*
+ ============================================================================
+ Name        : LaboratorioTarea.c
+ Author      : Facundo
+ Version     :
+ Copyright   : MIT
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+	int numero;
+
+	setbuf(stdout, NULL);
+	return EXIT_SUCCESS;
+}
